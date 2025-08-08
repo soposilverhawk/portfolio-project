df2517e3c6e74e8e77d20e2b54e4d8c753bf2782
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-08-08 22:12:35 +0400

    finish homepage and create button component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import devImg from "../public/homepage/developer-img.png";
+import Button from "@/components/Button/Button";
 
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className={styles.wrapper}>
+      <div className={styles.heroOverlay}></div>
+      <div className={styles.devInfo}>
+        <Image src={devImg} alt="placeholder alt"/>
+        <h1>John Doe</h1>
+        <h2>Programmer. Creative. Innovator</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+        </p>
+        <Button>Know more</Button>
+      </div>
     </div>
   );
 }
