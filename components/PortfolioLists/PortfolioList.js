827552f0cf5ff3846143389fc6afb614829eb63d
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-08-09 22:20:16 +0400

    add styling to PortfolioList component

--- components/PortfolioLists/PortfolioList.js
+++ components/PortfolioLists/PortfolioList.js
@@ -1,13 +1,14 @@
 "use client"
 import React from "react";
 import Image from "next/image";
+import styles from "./portfolioList.module.css"
 
 function PortfolioList({ data, variant }) {
   return (
-    <ul>
+    <ul className={styles.portfolioList}>
       {data.map(({ id, desc, icon, alt, link }) => (
-        <li key={id}>
-          <Image src={icon} alt={alt} />
+        <li key={id} className={variant === "navigation" && styles.infoItem}>
+          <Image src={icon} alt={alt}/>
           {desc}
           {link && variant === "contactList" && <span>{link}</span>}
         </li>
