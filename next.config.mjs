dd83c25286e5481852495f31e54fc5c0aafacb6e
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-08 03:40:23 +0400

    Remove basePath and assetPrefix from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/portfolio-project",      
-  assetPrefix: "/portfolio-project/",   
+  output: "export",  
 };
 
 export default nextConfig;
