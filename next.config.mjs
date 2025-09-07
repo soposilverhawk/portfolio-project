edb0270bb38165fbbc3ecdb695a2ba85672e3511
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-08 03:44:01 +0400

    Add basePath and assetPrefix back

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  
+  output: "export",
+  basePath: "/portfolio-project",      
+  assetPrefix: "/portfolio-project/",   
 };
 
 export default nextConfig;
